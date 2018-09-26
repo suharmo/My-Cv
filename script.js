0bda54671d333688341ee55b9947822f8370fb89
suharmo <soumaya.amri97@hotmail.fr>
2018-09-26 14:57:48 +0200

    last features js

---
+++ script.js
@@ -0,0 +1,12 @@
+prompt('How can I help You');
+alert('Call this number before anybody else engages me!! 0465180255');
+
+let myName = "amna"
+
+
+if (myName === 'Soumaya'){
+console.log('Love me right!')
+} else {
+    console.log('Go get it dude!')
+}
+
